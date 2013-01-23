baaf1f3a5b92626904a6ecc8c372398d10becb85
Steve McMahon <steve@dcn.org>
2013-01-23 09:11:34 -0800

    Fix cd problems on install:

--- manifests/install_plone.sh
+++ manifests/install_plone.sh
@@ -3,11 +3,15 @@
 PLONE_MAJOR=4.3
 PLONE_MINOR=4.3b1
 
+UI_OPTIONS="standalone --password=admin"
+
 UI_NAME=Plone-${PLONE_MINOR}-UnifiedInstaller
 PLONE_TARBALL=${UI_NAME}.tgz
 UI_URL=https://launchpad.net/plone/${PLONE_MAJOR}/${PLONE_MINOR}/+download/${PLONE_TARBALL}
 
 AS_VAGRANT="sudo -u vagrant"
+SHARED_DIR="/vagrant"
+VHOME="/home/vagrant"
 
 if [ ! -f $PLONE_TARBALL ]; then
     echo Downloading Plone Unified Installer
@@ -36,31 +40,32 @@ fi
 if [ ! -d Plone ]; then
     cd $UI_NAME
     echo Running Plone Unified Installer
-    $AS_VAGRANT ./install.sh standalone --password=admin --target=/home/vagrant/Plone
+    $AS_VAGRANT ./install.sh ${UI_OPTIONS} --target=${VHOME}/Plone
     if [ $? -gt 0 ]; then
         # remove partial install
         rm -r Plone
         echo Plone install failed
         exit 1
     fi
+    cd ${VHOME}
 fi
 
-if [ ! -d /vagrant/plone ]; then
+if [ ! -d ${SHARED_DIR}/plone ]; then
     echo Moving commonly edited source files into shared directory
     echo and linking them back to Plone instance.
-    $AS_VAGRANT mkdir /vagrant/plone
+    $AS_VAGRANT mkdir ${SHARED_DIR}/plone
 fi
 
-for fn in src buildout.cfg base.cfg
+for fn in src buildout.cfg base.cfg develop.cfg
 do
-    if [ ! -f /vagrant/plone/$fn ]; then
+    if [ ! -f ${SHARED_DIR}/plone/$fn ]; then
         echo $fn
-        mv Plone/zinstance/$fn /vagrant/plone
-        $AS_VAGRANT ln -s /vagrant/plone/$fn Plone/zinstance
+        mv Plone/zinstance/$fn ${SHARED_DIR}/plone
+        $AS_VAGRANT ln -s ${SHARED_DIR}/plone/$fn Plone/zinstance
     fi
 done
 
-for script in /vagrant/manifests/*.sh
+for script in ${SHARED_DIR}/manifests/*.sh
 do
     if [ ! -f `basename $script` ]; then
         $AS_VAGRANT cp $script .
